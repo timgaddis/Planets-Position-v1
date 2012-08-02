9b7a3c6fe65e9afd953e5722c923bcba44b6ebd1
Tim Gaddis <timgaddis@gmail.com>
2012-08-02 07:16:39 -0700

    Recompiled JNI code with version 8b NDK.

--- obj/local/armeabi/objs/planets_swiss/swiss/swemplan.o.d
+++ obj/local/armeabi/objs/planets_swiss/swiss/swemplan.o.d
@@ -1,137 +1,137 @@
 obj/local/armeabi/objs/planets_swiss/swiss/swemplan.o: \
  jni/swiss/swemplan.c \
- /media/Backup/Android/android-ndk-r8/platforms/android-14/arch-arm/usr/include/string.h \
- /media/Backup/Android/android-ndk-r8/platforms/android-14/arch-arm/usr/include/sys/cdefs.h \
- /media/Backup/Android/android-ndk-r8/platforms/android-14/arch-arm/usr/include/sys/cdefs_elf.h \
- /media/Backup/Android/android-ndk-r8/platforms/android-14/arch-arm/usr/include/malloc.h \
+ /media/Backup/Android/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/string.h \
+ /media/Backup/Android/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/sys/cdefs.h \
+ /media/Backup/Android/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/sys/cdefs_elf.h \
+ /media/Backup/Android/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/malloc.h \
  jni/swiss/swephexp.h jni/swiss/sweodef.h \
- /media/Backup/Android/android-ndk-r8/platforms/android-14/arch-arm/usr/include/math.h \
- /media/Backup/Android/android-ndk-r8/platforms/android-14/arch-arm/usr/include/sys/types.h \
- /media/Backup/Android/android-ndk-r8/platforms/android-14/arch-arm/usr/include/stdint.h \
- /media/Backup/Android/android-ndk-r8/platforms/android-14/arch-arm/usr/include/sys/_types.h \
- /media/Backup/Android/android-ndk-r8/platforms/android-14/arch-arm/usr/include/machine/_types.h \
- /media/Backup/Android/android-ndk-r8/platforms/android-14/arch-arm/usr/include/linux/posix_types.h \
- /media/Backup/Android/android-ndk-r8/platforms/android-14/arch-arm/usr/include/linux/stddef.h \
- /media/Backup/Android/android-ndk-r8/platforms/android-14/arch-arm/usr/include/linux/compiler.h \
- /media/Backup/Android/android-ndk-r8/platforms/android-14/arch-arm/usr/include/asm/posix_types.h \
- /media/Backup/Android/android-ndk-r8/platforms/android-14/arch-arm/usr/include/asm/types.h \
- /media/Backup/Android/android-ndk-r8/platforms/android-14/arch-arm/usr/include/linux/types.h \
- /media/Backup/Android/android-ndk-r8/platforms/android-14/arch-arm/usr/include/machine/kernel.h \
- /media/Backup/Android/android-ndk-r8/platforms/android-14/arch-arm/usr/include/sys/sysmacros.h \
- /media/Backup/Android/android-ndk-r8/platforms/android-14/arch-arm/usr/include/limits.h \
- /media/Backup/Android/android-ndk-r8/platforms/android-14/arch-arm/usr/include/sys/limits.h \
- /media/Backup/Android/android-ndk-r8/platforms/android-14/arch-arm/usr/include/linux/limits.h \
- /media/Backup/Android/android-ndk-r8/platforms/android-14/arch-arm/usr/include/machine/internal_types.h \
- /media/Backup/Android/android-ndk-r8/platforms/android-14/arch-arm/usr/include/machine/limits.h \
- /media/Backup/Android/android-ndk-r8/platforms/android-14/arch-arm/usr/include/sys/syslimits.h \
- /media/Backup/Android/android-ndk-r8/platforms/android-14/arch-arm/usr/include/stdlib.h \
- /media/Backup/Android/android-ndk-r8/platforms/android-14/arch-arm/usr/include/alloca.h \
- /media/Backup/Android/android-ndk-r8/platforms/android-14/arch-arm/usr/include/strings.h \
- /media/Backup/Android/android-ndk-r8/platforms/android-14/arch-arm/usr/include/memory.h \
- /media/Backup/Android/android-ndk-r8/platforms/android-14/arch-arm/usr/include/stdio.h \
- /media/Backup/Android/android-ndk-r8/platforms/android-14/arch-arm/usr/include/unistd.h \
- /media/Backup/Android/android-ndk-r8/platforms/android-14/arch-arm/usr/include/sys/select.h \
- /media/Backup/Android/android-ndk-r8/platforms/android-14/arch-arm/usr/include/sys/time.h \
- /media/Backup/Android/android-ndk-r8/platforms/android-14/arch-arm/usr/include/linux/time.h \
- /media/Backup/Android/android-ndk-r8/platforms/android-14/arch-arm/usr/include/signal.h \
- /media/Backup/Android/android-ndk-r8/platforms/android-14/arch-arm/usr/include/asm/signal.h \
- /media/Backup/Android/android-ndk-r8/platforms/android-14/arch-arm/usr/include/asm-generic/signal.h \
- /media/Backup/Android/android-ndk-r8/platforms/android-14/arch-arm/usr/include/asm/sigcontext.h \
- /media/Backup/Android/android-ndk-r8/platforms/android-14/arch-arm/usr/include/asm/siginfo.h \
- /media/Backup/Android/android-ndk-r8/platforms/android-14/arch-arm/usr/include/asm-generic/siginfo.h \
- /media/Backup/Android/android-ndk-r8/platforms/android-14/arch-arm/usr/include/sys/sysconf.h \
- /media/Backup/Android/android-ndk-r8/platforms/android-14/arch-arm/usr/include/linux/capability.h \
- /media/Backup/Android/android-ndk-r8/platforms/android-14/arch-arm/usr/include/pathconf.h \
- /media/Backup/Android/android-ndk-r8/platforms/android-14/arch-arm/usr/include/ctype.h \
+ /media/Backup/Android/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/math.h \
+ /media/Backup/Android/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/sys/types.h \
+ /media/Backup/Android/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/stdint.h \
+ /media/Backup/Android/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/sys/_types.h \
+ /media/Backup/Android/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/machine/_types.h \
+ /media/Backup/Android/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/linux/posix_types.h \
+ /media/Backup/Android/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/linux/stddef.h \
+ /media/Backup/Android/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/linux/compiler.h \
+ /media/Backup/Android/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/asm/posix_types.h \
+ /media/Backup/Android/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/asm/types.h \
+ /media/Backup/Android/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/linux/types.h \
+ /media/Backup/Android/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/machine/kernel.h \
+ /media/Backup/Android/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/sys/sysmacros.h \
+ /media/Backup/Android/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/limits.h \
+ /media/Backup/Android/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/sys/limits.h \
+ /media/Backup/Android/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/linux/limits.h \
+ /media/Backup/Android/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/machine/internal_types.h \
+ /media/Backup/Android/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/machine/limits.h \
+ /media/Backup/Android/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/sys/syslimits.h \
+ /media/Backup/Android/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/stdlib.h \
+ /media/Backup/Android/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/alloca.h \
+ /media/Backup/Android/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/strings.h \
+ /media/Backup/Android/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/memory.h \
+ /media/Backup/Android/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/stdio.h \
+ /media/Backup/Android/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/unistd.h \
+ /media/Backup/Android/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/sys/select.h \
+ /media/Backup/Android/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/sys/time.h \
+ /media/Backup/Android/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/linux/time.h \
+ /media/Backup/Android/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/signal.h \
+ /media/Backup/Android/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/asm/signal.h \
+ /media/Backup/Android/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/asm-generic/signal.h \
+ /media/Backup/Android/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/asm/sigcontext.h \
+ /media/Backup/Android/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/asm/siginfo.h \
+ /media/Backup/Android/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/asm-generic/siginfo.h \
+ /media/Backup/Android/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/sys/sysconf.h \
+ /media/Backup/Android/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/linux/capability.h \
+ /media/Backup/Android/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/pathconf.h \
+ /media/Backup/Android/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/ctype.h \
  jni/swiss/sweph.h jni/swiss/swephlib.h jni/swiss/swemptab.c
 
-/media/Backup/Android/android-ndk-r8/platforms/android-14/arch-arm/usr/include/string.h:
+/media/Backup/Android/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/string.h:
 
-/media/Backup/Android/android-ndk-r8/platforms/android-14/arch-arm/usr/include/sys/cdefs.h:
+/media/Backup/Android/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/sys/cdefs.h:
 
-/media/Backup/Android/android-ndk-r8/platforms/android-14/arch-arm/usr/include/sys/cdefs_elf.h:
+/media/Backup/Android/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/sys/cdefs_elf.h:
 
-/media/Backup/Android/android-ndk-r8/platforms/android-14/arch-arm/usr/include/malloc.h:
+/media/Backup/Android/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/malloc.h:
 
 jni/swiss/swephexp.h:
 
 jni/swiss/sweodef.h:
 
-/media/Backup/Android/android-ndk-r8/platforms/android-14/arch-arm/usr/include/math.h:
+/media/Backup/Android/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/math.h:
 
-/media/Backup/Android/android-ndk-r8/platforms/android-14/arch-arm/usr/include/sys/types.h:
+/media/Backup/Android/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/sys/types.h:
 
-/media/Backup/Android/android-ndk-r8/platforms/android-14/arch-arm/usr/include/stdint.h:
+/media/Backup/Android/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/stdint.h:
 
-/media/Backup/Android/android-ndk-r8/platforms/android-14/arch-arm/usr/include/sys/_types.h:
+/media/Backup/Android/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/sys/_types.h:
 
-/media/Backup/Android/android-ndk-r8/platforms/android-14/arch-arm/usr/include/machine/_types.h:
+/media/Backup/Android/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/machine/_types.h:
 
-/media/Backup/Android/android-ndk-r8/platforms/android-14/arch-arm/usr/include/linux/posix_types.h:
+/media/Backup/Android/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/linux/posix_types.h:
 
-/media/Backup/Android/android-ndk-r8/platforms/android-14/arch-arm/usr/include/linux/stddef.h:
+/media/Backup/Android/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/linux/stddef.h:
 
-/media/Backup/Android/android-ndk-r8/platforms/android-14/arch-arm/usr/include/linux/compiler.h:
+/media/Backup/Android/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/linux/compiler.h:
 
-/media/Backup/Android/android-ndk-r8/platforms/android-14/arch-arm/usr/include/asm/posix_types.h:
+/media/Backup/Android/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/asm/posix_types.h:
 
-/media/Backup/Android/android-ndk-r8/platforms/android-14/arch-arm/usr/include/asm/types.h:
+/media/Backup/Android/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/asm/types.h:
 
-/media/Backup/Android/android-ndk-r8/platforms/android-14/arch-arm/usr/include/linux/types.h:
+/media/Backup/Android/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/linux/types.h:
 
-/media/Backup/Android/android-ndk-r8/platforms/android-14/arch-arm/usr/include/machine/kernel.h:
+/media/Backup/Android/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/machine/kernel.h:
 
-/media/Backup/Android/android-ndk-r8/platforms/android-14/arch-arm/usr/include/sys/sysmacros.h:
+/media/Backup/Android/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/sys/sysmacros.h:
 
-/media/Backup/Android/android-ndk-r8/platforms/android-14/arch-arm/usr/include/limits.h:
+/media/Backup/Android/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/limits.h:
 
-/media/Backup/Android/android-ndk-r8/platforms/android-14/arch-arm/usr/include/sys/limits.h:
+/media/Backup/Android/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/sys/limits.h:
 
-/media/Backup/Android/android-ndk-r8/platforms/android-14/arch-arm/usr/include/linux/limits.h:
+/media/Backup/Android/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/linux/limits.h:
 
-/media/Backup/Android/android-ndk-r8/platforms/android-14/arch-arm/usr/include/machine/internal_types.h:
+/media/Backup/Android/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/machine/internal_types.h:
 
-/media/Backup/Android/android-ndk-r8/platforms/android-14/arch-arm/usr/include/machine/limits.h:
+/media/Backup/Android/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/machine/limits.h:
 
-/media/Backup/Android/android-ndk-r8/platforms/android-14/arch-arm/usr/include/sys/syslimits.h:
+/media/Backup/Android/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/sys/syslimits.h:
 
-/media/Backup/Android/android-ndk-r8/platforms/android-14/arch-arm/usr/include/stdlib.h:
+/media/Backup/Android/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/stdlib.h:
 
-/media/Backup/Android/android-ndk-r8/platforms/android-14/arch-arm/usr/include/alloca.h:
+/media/Backup/Android/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/alloca.h:
 
-/media/Backup/Android/android-ndk-r8/platforms/android-14/arch-arm/usr/include/strings.h:
+/media/Backup/Android/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/strings.h:
 
-/media/Backup/Android/android-ndk-r8/platforms/android-14/arch-arm/usr/include/memory.h:
+/media/Backup/Android/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/memory.h:
 
-/media/Backup/Android/android-ndk-r8/platforms/android-14/arch-arm/usr/include/stdio.h:
+/media/Backup/Android/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/stdio.h:
 
-/media/Backup/Android/android-ndk-r8/platforms/android-14/arch-arm/usr/include/unistd.h:
+/media/Backup/Android/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/unistd.h:
 
-/media/Backup/Android/android-ndk-r8/platforms/android-14/arch-arm/usr/include/sys/select.h:
+/media/Backup/Android/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/sys/select.h:
 
-/media/Backup/Android/android-ndk-r8/platforms/android-14/arch-arm/usr/include/sys/time.h:
+/media/Backup/Android/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/sys/time.h:
 
-/media/Backup/Android/android-ndk-r8/platforms/android-14/arch-arm/usr/include/linux/time.h:
+/media/Backup/Android/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/linux/time.h:
 
-/media/Backup/Android/android-ndk-r8/platforms/android-14/arch-arm/usr/include/signal.h:
+/media/Backup/Android/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/signal.h:
 
-/media/Backup/Android/android-ndk-r8/platforms/android-14/arch-arm/usr/include/asm/signal.h:
+/media/Backup/Android/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/asm/signal.h:
 
-/media/Backup/Android/android-ndk-r8/platforms/android-14/arch-arm/usr/include/asm-generic/signal.h:
+/media/Backup/Android/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/asm-generic/signal.h:
 
-/media/Backup/Android/android-ndk-r8/platforms/android-14/arch-arm/usr/include/asm/sigcontext.h:
+/media/Backup/Android/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/asm/sigcontext.h:
 
-/media/Backup/Android/android-ndk-r8/platforms/android-14/arch-arm/usr/include/asm/siginfo.h:
+/media/Backup/Android/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/asm/siginfo.h:
 
-/media/Backup/Android/android-ndk-r8/platforms/android-14/arch-arm/usr/include/asm-generic/siginfo.h:
+/media/Backup/Android/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/asm-generic/siginfo.h:
 
-/media/Backup/Android/android-ndk-r8/platforms/android-14/arch-arm/usr/include/sys/sysconf.h:
+/media/Backup/Android/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/sys/sysconf.h:
 
-/media/Backup/Android/android-ndk-r8/platforms/android-14/arch-arm/usr/include/linux/capability.h:
+/media/Backup/Android/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/linux/capability.h:
 
-/media/Backup/Android/android-ndk-r8/platforms/android-14/arch-arm/usr/include/pathconf.h:
+/media/Backup/Android/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/pathconf.h:
 
-/media/Backup/Android/android-ndk-r8/platforms/android-14/arch-arm/usr/include/ctype.h:
+/media/Backup/Android/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/ctype.h:
 
 jni/swiss/sweph.h:
 
